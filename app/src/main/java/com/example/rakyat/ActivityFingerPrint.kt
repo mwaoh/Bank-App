3375557662b77d1e8f6e1f2ceab0f4a709219f6e
Mwaura M <mwaurajoseph048@gmail.com>
2022-08-01 13:58:42 +0300

    designs

---
+++ app/src/main/java/com/example/rakyat/ActivityFingerPrint.kt
@@ -0,0 +1,11 @@
+package com.example.rakyat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityFingerPrint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finger_print)
+    }
+}
---
+++ app/src/main/java/com/example/rakyat/ActivitySignUp.kt
@@ -0,0 +1,11 @@
+package com.example.rakyat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+    }
+}
