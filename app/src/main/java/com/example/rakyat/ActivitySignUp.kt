adf2e548c9e243e03a4c7024e9caf87cf0f96d71
Mwaura M <mwaurajoseph048@gmail.com>
2022-08-09 21:50:28 +0300

    splashscreen + firebase

--- app/src/main/java/com/example/rakyat/ActivitySignUp.kt
+++ app/src/main/java/com/example/rakyat/ActivitySignUp.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ActivitySignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up)
+        setContentView(R.layout.activity_sign_up)
     }
 }
---
+++ app/src/main/java/com/example/rakyat/LaunchScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.example.rakyat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+
+
+@Suppress("DEPRECATION")
+class LaunchScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.launchscreenactivity)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 500) // 500 is the delayed time in milliseconds.
+    }
+}
+
+
